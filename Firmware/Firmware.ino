8e964d1ae156f0518400bf45ea3462250205866a
Dušan Simić <dusan.simic1810@gmail.com>
2024-06-25 15:05:34 +0200

    feat: Complete code

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -13,12 +13,134 @@
 //   Dušan Simić <dusan.simic@dmi.uns.ac.rs>
 //   Milana Medojević <medojevicm@uns.ac.rs>
 
+#include <Wire.h>
+#include "Adafruit_BMP280.h"
+#include "AHT10.h"
+#include <WiFi.h>
+#include <WiFiMulti.h>
+#include <InfluxDbClient.h>
+#include "config.h"
+
+
+Adafruit_BMP280 bmp;
+AHT10 AHT20(AHT10_ADDRESS_0X38, AHT20_SENSOR);
+
+WiFiMulti wifiMulti;
+
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_NAME);
+Point pointDevice("sensors");
+Point timesPointDevice("times");
+
+unsigned long loop_start;
+unsigned long start_reading;
+unsigned long start_sending;
+unsigned long iteration_complete;
+unsigned long loop_end;
+unsigned long iteration_count;
 
 void setup() {
   Serial.begin(115200);
+
+  while (!Serial);
+
+  WiFi.begin();
+  WiFi.mode(WIFI_STA);
+  wifiMulti.addAP(WIFI1_SSID, WIFI1_PASS);
+
+  client.setConnectionParamsV1(INFLUXDB_URL, INFLUXDB_NAME, INFLUXDB_USER, INFLUXDB_PASS);
+
+  pointDevice.addTag("device", INFLUXDB_TAG_DEVICE);
+  pointDevice.addTag("senslist", INFLUXDB_TAG_SENSOR_LIST);
+  timesPointDevice.addTag("ssid", WIFI1_SSID);
+
+  if (client.validateConnection()) {
+    Serial.print("Connected to InfluxDB: ");
+    Serial.println(client.getServerUrl());
+  } else {
+    Serial.print("InfluxDB connection failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+
+  if (!bmp.begin()) {
+    Serial.println("BMP280 initialization failed...");
+    while(1);
+  }
+
+  if (!AHT20.begin()) {
+    Serial.println("AHT20 initialization failed...");
+    while(1);
+  }
+
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,
+                  Adafruit_BMP280::SAMPLING_X2,
+                  Adafruit_BMP280::SAMPLING_X16,
+                  Adafruit_BMP280::FILTER_X16,
+                  Adafruit_BMP280::STANDBY_MS_500);
+  iteration_count = 0;
 }
 
 void loop() {
-  Serial.println("Hello SpliTech 2024 IoT Day!");
-  sleep(1);
+  loop_start = millis();
+
+  if (wifiMulti.run() != WL_CONNECTED) {
+    Serial.println("Not connected to WiFi!");
+    delay(1000);
+    return;
+  }
+
+  pointDevice.clearFields();
+  timesPointDevice.clearFields();
+
+  start_reading = millis();
+
+  double aht20temp = (double) AHT20.readTemperature();
+  pointDevice.addField("aht20temperature", aht20temp);
+
+  double aht20hum = (double) AHT20.readHumidity();
+  pointDevice.addField("aht20humidity", aht20hum);
+
+  double bmptemp = (double) bmp.readTemperature();
+  pointDevice.addField("bmptemperature", bmptemp);
+
+  double bmppress = (double) bmp.readPressure();
+  pointDevice.addField("bmppressure", bmppress);
+
+  double bmpalt = (double) bmp.readAltitude();
+  pointDevice.addField("bmpalt", bmpalt);
+  // TODO: Implement reading and adding a field for sensor values, for each
+  //       other value that is being recorded.
+  //       Functions for reading those values are:
+  //         AHT20.readHumidity()
+  //         bmp.readTemperature()
+  //         bmp.readPressure()
+  //         bmp.readAltitude()
+  //
+  //       We recommend using and modifying the previously implemented (above
+  //       this comment section).
+
+  start_sending = millis();
+
+  if (!client.writePoint(pointDevice)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+
+  iteration_complete = millis();
+  iteration_count++;
+
+  timesPointDevice.addField("rssi", WiFi.RSSI());
+  timesPointDevice.addField("read", start_calculating - start_reading);
+  timesPointDevice.addField("calc", start_sending - start_calculating);
+  timesPointDevice.addField("send", iteration_complete - start_sending);
+
+  if (!client.writePoint(timesPointDevice)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+
+  loop_end = millis();
+
+  if (loop_end - loop_start < DELAY_BEFORE) {
+    delay(DELAY_BEFORE - (loop_end - loop_start));
+  }
 }
--- Firmware/config.h
+++ Firmware/config.h
@@ -1,1 +1,14 @@
 // Define your configuration parameters here!
+
+#define WIFI1_SSID "IoTWorkshop"
+#define WIFI1_PASS "SpliTech2024"
+
+#define INFLUXDB_URL "http://192.168.0.10:8086"
+#define INFLUXDB_NAME "userXX"
+#define INFLUXDB_USER "userXX"
+#define INFLUXDB_PASS "userXX"
+
+#define INFLUXDB_TAG_DEVICE "esp32iottest"
+#define INFLUXDB_TAG_SENSOR_LIST "aht20,bmp280"
+
+#define DELAY_BEFORE 1000
